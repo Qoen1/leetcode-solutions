ffa65e016d0e6836587935d2ba6305e0032efcbf
k.verstappen <k.verstappen@pro-fa.com>
2023-10-29 21:58:51 +0100

    solved TwoSum puzzle

---
+++ TwoSum/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int[] ints = {
+    2,7,11,15
+};
+int target = 9;
+
+int[] arr = TwoSum(ints, target);
+string arrStr = "{\n";
+foreach (int i in arr)
+{
+    arrStr += " " + i.ToString() + ",\n";
+}
+arrStr += "}";
+
+Console.WriteLine(arrStr);
+
+int[] TwoSum(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        int num = nums[i];
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (j == i) break;
+
+            int toAdd = nums[j];
+            if (num + toAdd == target)
+            {
+                return new[] { i, j };
+            }
+        }
+    }
+
+    throw new Exception("sadge");
+}
